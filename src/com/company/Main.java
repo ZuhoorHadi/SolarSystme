aeba83012aca597b3728de50a5a038b413599175
ZuhoorHadi <zuhoorhadi@gmail.com>
2022-05-15 14:22:54 -0400

    Solar System Basic Ready

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        SolarSystem factorX= new SolarSystem();
+
+        factorX.sun="1";
+        factorX.moon="2";
+        factorX.planet="2";
+        factorX.stars="1000";
+
+
+    }
+}
